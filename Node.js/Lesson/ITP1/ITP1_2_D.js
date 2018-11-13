3acbd5ae3b8aa2bfa018908839ebcdd7198832a8
38fanjia <38fanjia+github@gmail.com>
2018-11-13 22:08:33 +0900

    :bug: fix ITP1_2_D

--- Node.js/Lesson/ITP1/ITP1_2_D.js
+++ Node.js/Lesson/ITP1/ITP1_2_D.js
@@ -1,25 +1,9 @@
 // 2_D : Circle in a Rectangle
 process.stdin.setEncoding('utf8')
 
-// W, H が正数になるよう変換する
-var convertPositiveNumber = function (W, H, x, y) {
-  if (W < 0) {
-    x += W = -W
-  }
-  if (H < 0) {
-    y += H = -H
-  }
-
-  return [W, H, x, y]
-}
-
 process.stdin.on('data', input => {
   let [W, H, x, y, r] = input.split(' ').map(value => parseInt(value, 10))
   let answer = true
-
-  if (W < 0 || H < 0) {
-    [W, H, x, y] = convertPositiveNumber(W, H, x, y)
-  }
 
   if (x - r < 0 || x + r > W) {
     answer = false
