a095552fc624632886f86637c5d5aaa01f29a024
fanjia38 <38fanjia+github@gmail.com>
2019-07-23 22:05:07 +0900

    :sparkles: add ITP1_10_D - https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/10/ITP1_10_D

---
+++ JavaScript/Lesson/ITP1/10/ITP1_10_D.js
@@ -0,0 +1,29 @@
+/**
+ * 10_D : Distance II
+ * https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/10
+ */
+function calc (dataMap, p) {
+  const manh = dataMap.reduce((prev, current) => {
+    return prev + Math.pow(Math.abs(current.x - current.y), p)
+  }, 0)
+  return Math.pow(manh, 1 / p)
+}
+
+function call (input) {
+  const dataList = input.split(/\n/)
+  const n = parseInt(dataList[0])
+  const x = dataList[1].split(' ', n).map(val => parseInt(val))
+  const y = dataList[2].split(' ', n).map(val => parseInt(val))
+
+  const dataMap = x.map((val, index) => {
+    return { x: parseInt(val), y: parseInt(y[index]) }
+  })
+
+  console.log(calc(dataMap, 1))
+  console.log(calc(dataMap, 2))
+  console.log(calc(dataMap, 3))
+  console.log(Math.max(...dataMap.map(val => calc([val], 1))))
+}
+
+// input
+call(require('fs').readFileSync('/dev/stdin', 'utf8'))
